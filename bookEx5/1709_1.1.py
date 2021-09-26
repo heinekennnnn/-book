6030c5b70f827d85da0245b89c253b15a44d86cf
heinekennnnn <63130027@kmitl.ac.th>
2021-09-26 16:02:52 +0700

    ex5

---
+++ bookEx5/1709_1.1.py
@@ -0,0 +1,5 @@
+for x in range(200):
+      if ("fgh" in "dgdhjdghdf"):
+             print("1")
+      else:
+             print("-1")
---
+++ bookEx5/1709_1.2.py
@@ -0,0 +1,5 @@
+for x in range(200):
+      if ("hhfhf" in "ghhfhfdjdu"):
+           print("1")
+      else:
+           print("-1")
